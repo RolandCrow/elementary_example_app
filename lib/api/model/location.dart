10d0f0247fceb333b98bbea1c5742d20375bb199
Yevgeniy <Razvivatel@gmail.com>
2022-08-04 15:41:05 +0300

    Initial commit

---
+++ lib/api/model/location.dart
@@ -0,0 +1,52 @@
+import 'package:json_annotation/json_annotation.dart';
+
+import 'location_type.dart';
+part 'location.g.dart';
+
+@JsonSerializable()
+class Location {
+  final String title;
+  @JsonKey(name: 'location_type')
+  final LocationType locationType;
+  @JsonKey(name: 'latt_long')
+  @LatLngConverter()
+  final LatLng latLng;
+  final int woeid;
+
+  const Location({
+    required this.title,
+    required this.locationType,
+    required this.latLng,
+    required this.woeid,
+});
+
+  
+}
+
+
+class LatLng {
+  final double latitude;
+  final double longitude;
+
+  const LatLng({required this.latitude, required this.longitude});
+
+}
+
+class LatLngConverter implements JsonConverter<LatLng, String> {
+  const LatLngConverter();
+
+
+  @override
+  LatLng fromJson(String jsonString) {
+    final parts = jsonString.split(',');
+    return LatLng(
+        latitude: double.parse(parts.first) ?? 0,
+        longitude: double.parse(parts[1]) ?? 0);
+  }
+
+  @override
+  String toJson(LatLng latLng) {
+   return '${latLng.latitude}, ${latLng.longitude}';
+  }
+}
+
---
+++ lib/api/model/weather.dart
@@ -0,0 +1,89 @@
+import 'package:json_annotation/json_annotation.dart';
+
+import 'weather_state.dart';
+
+part 'weather.g.dart';
+
+
+
+@JsonSerializable()
+class Weather {
+  final int id;
+  @JsonKey(name: 'weather_state_name')
+  final String weatherStateName;
+  @JsonKey(name: 'weather_state_abbr', unknownEnumValue: WeatherState.unknown)
+  final WeatherState weatherStateAbbr;
+  @JsonKey(name: 'wind_direction_compass',
+          unknownEnumValue: WindDirectionCompass.unknown
+  )
+  final WindDirectionCompass windDirectionCompass;
+  final DateTime created;
+  @JsonKey(name: 'applicable_date')
+  final DateTime applicableDate;
+  @JsonKey(name: 'min_temp')
+  final double minTemp;
+  @JsonKey(name: 'max_temp')
+  final double maxTemp;
+  @JsonKey(name: 'the_temp')
+  final double theTemp;
+  @JsonKey(name: 'wind_speed')
+  final double windSpeed;
+  @JsonKey(name: 'wind_direction')
+  final double windDirection;
+  @JsonKey(name: 'air_pressure')
+  final double airPressure;
+  final int humidity;
+  final double visibility;
+  final int predictability;
+
+  const Weather({
+    required this.id,
+    required this.weatherStateName,
+    required this.windDirectionCompass,
+    required this.weatherStateAbbr,
+    required this.created,
+    required this.applicableDate,
+    required this.minTemp,
+    required this.maxTemp,
+    required this.theTemp,
+    required this.windSpeed,
+    required this.windDirection,
+    required this.airPressure,
+    required this.humidity,
+    required this.visibility,
+    required this.predictability,
+});
+
+
+  factory Weather.fromJson(Map<String, dynamic> json ) =>
+      _$WeatherFromJson(json);
+
+  @override
+  String toString() {
+    return 'Weather{id: $id, weatherStateName: $weatherStateName, weatherStateAbbr: $weatherStateAbbr, windDirectionCompass: $windDirectionCompass, created: $created, applicableDate: $applicableDate, minTemp: $minTemp, maxTemp: $maxTemp, theTemp: $theTemp, windSpeed: $windSpeed, windDirection: $windDirection, airPressure: $airPressure, humidity: $humidity, visibility: $visibility, predictability: $predictability}';
+  }
+}
+
+
+enum WindDirectionCompass {
+
+  @JsonValue('N')
+  north,
+  @JsonValue('NE')
+  northEast,
+  @JsonValue('E')
+  east,
+  @JsonValue('SE')
+  southEast,
+  @JsonValue('S')
+  south,
+  @JsonValue('SW')
+  southWest,
+  @JsonValue('W')
+  west,
+  @JsonValue('NW')
+  northWest,
+  unknown,
+
+
+}
